b946917ee5892134cb0266cee76ab1663e7d3078
Celina <ribeiro.f.celina@gmail.com>
2023-12-01 12:34:46 -0300

    validação campo no create

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -9,11 +9,13 @@
       <div class="row m-2">
         <div class="col-md-10 mt-2">
           <label for="subject">Assunto</label>
-          <input type="text" class="form-control" name="subject">
+          <input type="text" class="form-control {{ $errors->has('subject') ? 'is-invalid' : '' }}" name="subject" value="{{old('subject')}}">
+          <div class="invalid-feedback">{{ $errors->first('subject')}}</div>
         </div>
         <div class="col-md-10 mt-2">
           <label for="body">Descrição</label>
-          <textarea name="body" class="form-control" cols="30" rows="10"></textarea>
+          <textarea name="body" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" cols="30" rows="10">{{old('body')}}</textarea>
+          <div class="invalid-feedback">{{ $errors->first('body')}}</div>
         </div>
       </div>
       <button type="submit" class="btn btn-primary m-2">Salvar</button>
